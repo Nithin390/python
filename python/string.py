70b6fb5caf14c8561ee3940f5ded1a0c44379352
Nithin390 <112631306+Nithin390@users.noreply.github.com>
2022-09-07 22:03:09 -0700

    Add files via upload

--- python/string.py
+++ python/string.py
@@ -1,11 +1,9 @@
-s=input("enter a string:")
-d=l=0
-for c in s:
-    if c.isdigit():
-        d=d+1
-    elif c.isalpha():
-        l=l+1
-    else:
-          pass
-print("letters",1)
-print("Digits", d)
+a = str(input("enter first string : "))
+b = str(input("enter second string : "))
+strlength = len(a)
+count = 0
+for i in range(strlength):
+    if(a[i] == b[i]):
+        count +=1
+print("number of matches : ",count)
+ 
